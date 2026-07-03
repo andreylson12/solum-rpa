671817c6b972cc71ac3956b9ec692ace77ab922c
andreylson12 <Andreylsonvirtual@gmail.com>
2026-07-03 11:14:04 -0300

    Update parser-manager.js

--- parsers/parser-manager.js
+++ parsers/parser-manager.js
@@ -1,1 +1,55 @@
+(function(){
+  window.SOLUM = window.SOLUM || {};
+  if(SOLUM.parsers) return;
 
+  const ParserManager = {
+    lista: [],
+
+    registrar(parser){
+      if(!parser || !parser.nome || typeof parser.identificar !== 'function' || typeof parser.extrair !== 'function'){
+        throw new Error('Parser inválido.');
+      }
+
+      this.lista.push(parser);
+      SOLUM.engine.log('Parser registrado: ' + parser.nome, 'ok');
+    },
+
+    identificar(texto){
+      for(const parser of this.lista){
+        try{
+          if(parser.identificar(texto)){
+            SOLUM.engine.log('Parser selecionado: ' + parser.nome, 'ok');
+            return parser;
+          }
+        }catch(e){
+          SOLUM.engine.log('Erro no parser ' + parser.nome, 'erro');
+        }
+      }
+
+      SOLUM.engine.log('Nenhum parser específico encontrado. Usando genérico.', 'info');
+      return null;
+    },
+
+    extrair(texto){
+      const parser = this.identificar(texto);
+
+      if(parser){
+        return parser.extrair(texto);
+      }
+
+      return {
+        transportadora: 'NÃO IDENTIFICADA',
+        motorista: '',
+        cpfMotorista: '',
+        placaCavalo: '',
+        placaCarreta1: '',
+        placaCarreta2: '',
+        uf: '',
+        tipoVeiculo: '',
+        textoOriginal: texto
+      };
+    }
+  };
+
+  SOLUM.parsers = ParserManager;
+})();
