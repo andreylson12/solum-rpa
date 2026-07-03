42af9c3eb7a068b58ea241dd6efba595445bef3d
andreylson12 <Andreylsonvirtual@gmail.com>
2026-07-03 14:38:19 -0300

    Update processo.js

--- core/processo.js
+++ core/processo.js
@@ -1,1 +1,61 @@
+(function(){
 
+window.SOLUM = window.SOLUM || {};
+
+if(SOLUM.processo) return;
+
+const Processo={
+
+    dados:{
+
+        ticket:{},
+
+        xml:{},
+
+        planilha:{},
+
+        ordem:{},
+
+        pesagem:{},
+
+        laudo:{},
+
+        preenchimento:{},
+
+        erros:[]
+
+    },
+
+    salvar(modulo,dados){
+
+        this.dados[modulo]=dados;
+
+        SOLUM.engine.log(
+            "Processo atualizado: "+modulo,
+            "ok"
+        );
+
+    },
+
+    obter(modulo){
+
+        return this.dados[modulo];
+
+    },
+
+    adicionarErro(erro){
+
+        this.dados.erros.push(erro);
+
+        SOLUM.engine.log(
+            erro,
+            "erro"
+        );
+
+    }
+
+};
+
+SOLUM.processo=Processo;
+
+})();
