4310276d973a59e76cfc5484a14fb75d47799b8b
andreylson12 <Andreylsonvirtual@gmail.com>
2026-07-03 15:03:44 -0300

    Update primeira-tela.js

--- config/primeira-tela.js
+++ config/primeira-tela.js
@@ -1,1 +1,21 @@
+(function(){
+  window.SOLUM = window.SOLUM || {};
+  SOLUM.config = SOLUM.config || {};
 
+  SOLUM.config.primeiraTela = {
+    unidadeNegocio: 'Armazém',
+    processo: 'Entrada',
+    tipoTransporte: 'MERCOSUL',
+    operacao: '4 - RECEPÇÃO COMPRA PJ/PF - MOD55',
+    material: 'SOJA EM GRAOS CLASSE AMARELA',
+    safra: '2025/2026',
+    deposito: 'FOB',
+
+    naoPreencher: [
+      'Transportadora',
+      'D. Destino',
+      'Observação',
+      'Local de Armazenagem'
+    ]
+  };
+})();
