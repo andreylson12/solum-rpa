2d80ff29c25485a770337a506c81ebc01c7c34a9
andreylson12 <Andreylsonvirtual@gmail.com>
2026-07-04 08:11:03 -0300

    Update context.js

--- core/context.js
+++ core/context.js
@@ -1,1 +1,40 @@
+(function(){
+  window.SOLUM = window.SOLUM || {};
+  if(SOLUM.context) return;
 
+  SOLUM.context = {
+    ticket:{},
+    arquivos:{},
+    textos:{},
+
+    dados:{
+      xml:{},
+      planilha:{},
+      ordem:{},
+      laudo:{},
+      pesagem:{}
+    },
+
+    validacao:{
+      ordem:null,
+      geral:null
+    },
+
+    reset(){
+      this.ticket = {};
+      this.arquivos = {};
+      this.textos = {};
+      this.dados = {
+        xml:{},
+        planilha:{},
+        ordem:{},
+        laudo:{},
+        pesagem:{}
+      };
+      this.validacao = {
+        ordem:null,
+        geral:null
+      };
+    }
+  };
+})();
